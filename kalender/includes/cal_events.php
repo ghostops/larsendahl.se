c8b6c1cdead7cf13611ffcdb8bbe48f85472f861
ghostops <ludvig@ghostops.nu>
2020-08-24 14:39:11 +0200

    remove unused

--- kalender/includes/cal_events.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	// Loader - class and connection
-	include('loader.php');
-	
-	echo $calendar->json_transform();
-		
-?>
--- kalender/index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-	header("Location: https://kalender.larsendahl.se");
- ?>
